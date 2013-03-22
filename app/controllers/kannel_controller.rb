17885819cf29bc48a5e2b2b5070029516328571a
Marvin Arnold <marvinmarnold@gmail.com>
2013-03-22 16:54:47 +0200

    Mid day rush

--- app/controllers/kannel_controller.rb
+++ app/controllers/kannel_controller.rb
@@ -5,7 +5,7 @@ class KannelController < ApplicationController
     sent_at = params[:time]
     sent_at ||= Time.now
     body = params[:message]
-    
+
     recipient_number ||= "Movercado"
     recipient_phone = Phone.find_or_create_by_number(recipient_number)
     receiver = recipient_phone.user
@@ -14,7 +14,7 @@ class KannelController < ApplicationController
     sender_phone = Phone.find_or_create_by_number(sender_number)
     sender = sender_phone.user
     sender ||= sender_phone.create_user
-    
+
     Sms.create!(
       sender_phone_id: sender_phone.id,
       recipient_phone_id: recipient_phone.id,
--- app/models/sms.rb
+++ app/models/sms.rb
@@ -1,21 +1,21 @@
 class Sms < ActiveRecord::Base
   belongs_to :app
   attr_accessible :body, :recipient_phone_id, :sender_phone_id, :sent_at
-  
+
   def process
     app = App.find_by_code(app_code)
     app ||= App.find_by_code(sanitized_app_code)
     app.process(self) unless app.blank?
   end
-  
+
   def app_code
     Sms.app_code(body)
   end
-  
+
   def sanitized_app_code
     Sms.app_code(Sms.sanitize_for_codes(body))
   end
-  
+
   def self.app_code(message_text)
     match = message_text.match(/^(\s*\S{1}){3}/)
     (match) ? match[0] : match
