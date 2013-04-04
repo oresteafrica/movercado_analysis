0bc123ca3551fa046e60c762e875b2095f504236
Oreste <oreste@oreste-System-Product-Name>
2013-04-04 05:11:15 +0200

    final submission

--- app/mailers/user_alert.rb
+++ app/mailers/user_alert.rb
@@ -2,7 +2,7 @@ class UserAlert < ActionMailer::Base
   default from: "movercado@oreste.parlatano.org"
 
 def alert_email()
-mail to: 'oreste@parlatano.org,macparlant@yahoo.com',
+mail to: 'oreste@parlatano.org,movercado-jobs@psi.org.mz',
 subject: "Final movercado-analysis submission",
 body: "Message sent from Oreste Parlatano oreste@parlatano.org, github https://github.com/oresteafrica/movercado_analysis, heroku http://pure-wildwood-1773.herokuapp.com/"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,35 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-1001.times do
+# 1001.times do
 
-thinter = Interaction.create(name: 'Troca Aki Test',user_id: 3,app_id:1)
+# thinter = Interaction.create(name: 'Troca Aki Test',user_id: 3,app_id:1)
 
+# end
+
+
+t = TrocaAkiValidation.where(name: "Troca Aki Campaign", code: "TTT").first_or_create
+i = IpcValidation.where(name: "IPC Validation Campaign", code: "III").first_or_create
+
+Role.find_or_create_by_name("vendor")
+Role.find_or_create_by_name("activista")
+
+30.times do
+  u = User.create!
+  u.roles.create(app_id: i.id, name: "activista")
+# http://www.ruby-forum.com/topic/93106
+  rand(15).times do
+    c = Code.create(app_id: i.id, user_id: u.id)
+
+    Sms.receive(
+# http://www.ruby-doc.org/stdlib-1.9.3/libdoc/securerandom/rdoc/SecureRandom.html
+      SecureRandom.hex,
+      "Movercado",
+      c.to_s
+    )
+
+  end
 end
+
+
+
